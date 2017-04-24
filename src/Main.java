a0f10bc6798fe9619b8f7600c5d60c167903bdac
Adam Balint <ab11fl@brocku.ca>
2017-04-24 02:30:30 -0400

    Reduced number of runs per parameter set and reduced number of parameter sets

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
 		File f = new File ("Logs/"+nanoTime);
 		f.mkdirs();
 		
-		for (int run = 0; run < 20; run++){
+		for (int run = 0; run < 10; run++){
 			Variables.currentRun = run;
 			f = new File ("Logs/"+nanoTime+"/Run-"+run);
 			f.mkdir();
